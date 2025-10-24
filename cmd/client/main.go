397c504ce6935b7b0888c82d3a222031ba9b37ae
Matt Horst <mattahorst@gmail.com>
2025-10-24 09:53:35 -0600

    creates transient queue on client start

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	ampq "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := ampq.Dial(url)
+	if err != nil {
+		log.Fatalln("Failed to connect to rabbitmq")
+	}
+
+	userName, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalln("Failed to get valid username")
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, userName),
+		routing.PauseKey,
+		pubsub.QueueTypeTransient,
+	)
+
+	if err != nil {
+		log.Fatalln("Failed to declare and bind transient queue")
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+	<- sigCh
+
+	fmt.Println("Server shutting down...")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -8,6 +8,13 @@ import (
 	ampq "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType int
+
+const (
+	QueueTypeDurable SimpleQueueType = iota
+	QueueTypeTransient
+)
+
 func PublishJSON[T any] (ch *ampq.Channel, exchange, key string, val T) error {
 	json, err := json.Marshal(val)
 	if err != nil {
@@ -30,4 +37,27 @@ func PublishJSON[T any] (ch *ampq.Channel, exchange, key string, val T) error {
 	}
 
 	return nil
+}
+
+func DeclareAndBind(
+	conn *ampq.Connection,
+	exchange string,
+	queueName string,
+	key string,
+	queueType SimpleQueueType,
+) (*ampq.Channel, ampq.Queue, error){
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, ampq.Queue{}, fmt.Errorf("Failed to create channel: %v", err)
+	}
+
+	isDurable := queueType == QueueTypeDurable
+	isAutoDelete := queueType == QueueTypeTransient
+	isExclusive := queueType == QueueTypeTransient
+	q, err := ch.QueueDeclare(queueName, isDurable, isAutoDelete, isExclusive, false, nil)
+	if err != nil {
+		return nil, ampq.Queue{}, fmt.Errorf("Failed to declare queue: %v", err)
+	}
+
+	return ch, q, nil
 }
