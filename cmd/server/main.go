3d331a817c99713d0bafdd3c11cdd76ed1068183
Matt Horst <mattahorst@gmail.com>
2025-10-24 01:17:39 -0600

    allows server program to connect to rabbitmq and await signal interrupt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatal("Failed to connect to rabbitmq")
+	}
+	defer conn.Close()
+
+	fmt.Println("Successfully connected to rabbitmq")
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<- ch
+
+	fmt.Println("Server shutting down...")
 }
